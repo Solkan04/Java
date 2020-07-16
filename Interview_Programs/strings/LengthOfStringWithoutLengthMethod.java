5bbb42fbb653f123e7fe54e9273abadf403008fa
Solkan <solkanmahesh@gmail.com>
2020-07-16 22:52:25 +0530

    Second Commit

---
+++ Interview_Programs/strings/LengthOfStringWithoutLengthMethod.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class LengthOfStringWithoutLengthMethod 
+{
+	public static void main(String[] args) 
+	{
+		String s="mahesh";
+		char c[]=s.toCharArray();
+		int i=0;
+		for(char r:c)
+			i++;
+		System.out.println("Length of String is: " +i);
+	}
+}
