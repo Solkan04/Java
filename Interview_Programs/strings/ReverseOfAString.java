7268385a59c65acea06c1db9136d11a69ec5c8db
Solkan <solkanmahesh@gmail.com>
2020-07-16 22:35:05 +0530

    First Commit

---
+++ Interview_Programs/strings/ReverseOfAString.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class ReverseOfAString 
+{
+	public static void main(String[] args) 
+	{
+		StringBuffer s=new StringBuffer("Kengeri");
+		StringBuffer rs=s.reverse();
+		System.out.println(rs);
+	}
+}
