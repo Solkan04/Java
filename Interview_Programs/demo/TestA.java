45219231534f566856f0663b9c492b8993e424b9
Mahesh <maheshsc04@gmail.com>
2020-07-18 15:41:47 +0530

    Modified TestA.Java

--- Interview_Programs/demo/TestA.java
+++ Interview_Programs/demo/TestA.java
@@ -1,10 +1,21 @@
 package demo;
 
-public class TestA {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class TestA 
+{
+	public static void main(String[] args) 
+	{
+		TestA.add(2, 3);
+		TestA.multi(3, 4);
 	}
 
+	static void add(int a, int b)
+	{
+		int res=a+b;
+		System.out.println(res);
+	}
+	static void multi(int x, int y)
+	{
+		int res=x*y;
+		System.out.println(res);
+	}
 }
