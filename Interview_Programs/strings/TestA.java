df83770476209d6aa90107c7669449890fa3d4e1
Mahesh <maheshsc04@gmail.com>
2020-07-18 12:18:19 +0530

    Modified TestA

--- Interview_Programs/strings/TestA.java
+++ Interview_Programs/strings/TestA.java
@@ -1,10 +1,9 @@
 package strings;
 
-public class TestA {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class TestA 
+{
+	public static void main(String args[])
+	{
+		System.out.println();
 	}
-
 }
