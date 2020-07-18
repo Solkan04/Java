4d75bf0d78461c7677e6a275bb78a6ba16f9ffb0
Mahesh <maheshsc04@gmail.com>
2020-07-18 16:23:01 +0530

    Modified TestB.Java

--- Interview_Programs/demo/TestB.java
+++ Interview_Programs/demo/TestB.java
@@ -1,10 +1,14 @@
 package demo;
 
-public class TestB {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class TestB 
+{
+	public static void main(String[] args) 
+	{
+		TestB b=new TestB();
+		b.test();
 	}
-
+	public void test()
+	{
+		System.out.println("Test");
+	}
 }
