d42a2aef0f4e2a381ebbef09221722e5746b88ea
Mahesh <maheshsc04@gmail.com>
2020-07-18 18:02:47 +0530

    Modified TestC.Java & Added new file TestD.Java

--- Interview_Programs/demo/TestC.java
+++ Interview_Programs/demo/TestC.java
@@ -1,10 +1,24 @@
 package demo;
 
-public class TestC {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+class Sample
+{
+	private int salary;
+	void setPrivate(int salary)
+	{
+		this.salary=salary;
 	}
-
+	int getPrivate()
+	{
+		return salary;
+	}
+}
+public class TestC 
+{
+	public static void main(String[] args) 
+	{
+		Sample s=new Sample();
+		s.setPrivate(900000);
+		int res=s.getPrivate();
+		System.out.println(res);
+	}
 }
