dbc1323ffb6304ffb125474766e0436f6917f73e
Solkan <solkanmahesh@gmail.com>
2020-07-18 16:20:16 +0530

    Modified TestB.Java

--- Interview_Programs/demo/TestB.java
+++ Interview_Programs/demo/TestB.java
@@ -1,10 +1,14 @@
 package demo;
 
-public class TestB {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class TestB 
+{
+	public static void main(String[] args) 
+	{
+		TestB b=new TestB();
+		b.test1();
 	}
-
+	public void test1()
+	{
+		System.out.println("Lead");
+	}
 }
