ac2c65c677879351d48501ff8b591b8ceb76f2ab
Mahesh <maheshsc04@gmail.com>
2020-07-18 10:10:35 +0530

    Modified LeadA.java

--- Interview_Programs/strings/LeadA.java
+++ Interview_Programs/strings/LeadA.java
@@ -1,10 +1,9 @@
 package strings;
 
-public class LeadA {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class LeadA 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println();
 	}
-
 }
